8db35dcbfeb6881127b94e87d1818c4cacd65aad
Dylan <42679126+burghporter314@users.noreply.github.com>
2026-03-04 11:58:22 -0500

    Fixed bug with container that was not exposing localhost

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == "__main__":
 
     print(app.url_map)
-    app.run(debug=True, port=5050, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=5050, use_reloader=False)
